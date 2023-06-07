b07a4c42fa531344b1946471c561722bafb88f4e
Kasper Andkjær <kasper@coteries.com>
2023-06-08 01:48:40 +0200

    Abstracted Basic endpoint tests for same trick as last commit.

---
+++ src/test/java/com/example/ContactsAPI/BaseEndpointTests.java
@@ -0,0 +1,216 @@
+package com.example.ContactsAPI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import com.example.ContactsAPI.models.DBObject;
+import com.example.ContactsAPI.models.DTObject;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+abstract class BaseEndpointTests<DB extends DBObject, DT extends DTObject, R extends CrudRepository<DB, Long>> {
+    protected final String API_ROOT;
+
+    protected BaseEndpointTests(String API_ROOT) {
+        this.API_ROOT = API_ROOT;
+    }
+
+    @Autowired
+    protected R repo;
+
+    @LocalServerPort
+    protected int port;
+
+    @BeforeEach
+    public void setPort() {
+        RestAssured.port = this.port;
+        repo.deleteAll();
+    }
+
+    protected abstract DT createRandomObject();
+
+    protected abstract DT createInvalidObject();
+
+    protected String getEntryUrl(Response response) {
+        return response.getHeader("location");
+    }
+
+    protected String getRandomEntryUrl() {
+        return API_ROOT + "/" + RandomStringUtils.randomNumeric(10);
+    }
+
+    // C
+    @Test
+    void createValid_singleEntry_then201() {
+        // Given
+        DT contact = createRandomObject();
+
+        // When
+        Response createResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact)
+                .post(API_ROOT);
+
+        // Then
+        assertEquals(HttpStatus.CREATED.value(), createResponse.getStatusCode());
+    }
+
+    @Test
+    void createInvalid_contact_then400() {
+        // Given
+        DT contact = createInvalidObject();
+
+        // When
+        Response createResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact).post(API_ROOT);
+
+        // Then
+        assertEquals(HttpStatus.BAD_REQUEST.value(), createResponse.getStatusCode());
+    }
+
+    // R
+    @Test
+    void readFound_singleEntry_then200() {
+        // Given
+        DT contact = createRandomObject();
+        Response createResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact).post(API_ROOT);
+
+        // When
+        Response getResponse = RestAssured.get(getEntryUrl(createResponse));
+
+        // Then
+        assertEquals(HttpStatus.OK.value(), getResponse.getStatusCode());
+    }
+
+    @Test
+    void readMissing_singleEntry_then404() {
+        // Given
+
+        // When
+        Response getResponse = RestAssured.get(getRandomEntryUrl());
+
+        // Then
+        assertEquals(HttpStatus.NOT_FOUND.value(), getResponse.getStatusCode());
+    }
+
+    @Test
+    void readFound_entryList_then200() {
+        // Given
+        DT contact = createRandomObject();
+        DT contact2 = createRandomObject();
+        RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact).post(API_ROOT);
+        RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact2).post(API_ROOT);
+
+        // When
+        Response getResponse = RestAssured.get(API_ROOT);
+
+        // Then
+        assertEquals(HttpStatus.OK.value(), getResponse.getStatusCode());
+        assertEquals(2, getResponse.body().as(Object[].class).length);
+    }
+
+    @Test
+    void readNone_entryList_then204() {
+        // Given
+
+        // When
+        Response getResponse = RestAssured.get(API_ROOT);
+
+        // Then
+        assertEquals(HttpStatus.NO_CONTENT.value(), getResponse.getStatusCode());
+    }
+
+    // U
+    @Test
+    void updateWithValid_singleEntry_then200() {
+        // Given
+        DT contact = createRandomObject();
+        DT contact2 = createRandomObject();
+
+        // When
+        Response createResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact).post(API_ROOT);
+        Response updateResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact2).put(getEntryUrl(createResponse));
+
+        // Then
+        assertEquals(HttpStatus.OK.value(), updateResponse.getStatusCode());
+    }
+
+    @Test
+    void updateMissing_singleEntry_then201() {
+        // Given
+        DT contact = createRandomObject();
+
+        // When
+        Response updateResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact).put(getRandomEntryUrl());
+
+        // Then
+        assertEquals(HttpStatus.CREATED.value(), updateResponse.getStatusCode());
+    }
+
+    @Test
+    void UpdateWithInvalid_singleEntry_then400() {
+        // Given
+        DT contact = createInvalidObject();
+
+        // When
+        Response updateResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact).put(getRandomEntryUrl());
+
+        // Then
+        assertEquals(HttpStatus.BAD_REQUEST.value(), updateResponse.getStatusCode());
+    }
+
+    // D
+    @Test
+    void deleteExisting_singleEntry_then200() {
+        // Given
+        DT contact = createRandomObject();
+        Response createResponse = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(contact).post(API_ROOT);
+
+        String contactLoc = getEntryUrl(createResponse);
+
+        // When
+        Response deleteResponse = RestAssured.delete(contactLoc);
+
+        // Then
+        assertEquals(HttpStatus.OK.value(), deleteResponse.getStatusCode());
+        Response getResponse = RestAssured.get(contactLoc);
+        assertEquals(HttpStatus.NOT_FOUND.value(), getResponse.getStatusCode());
+    }
+
+    @Test
+    void deleteMissing_singleEntry_then404() {
+        // Given
+
+        // When
+        Response deleteResponse = RestAssured.delete(getRandomEntryUrl());
+
+        // Then
+        assertEquals(HttpStatus.NOT_FOUND.value(), deleteResponse.getStatusCode());
+    }
+}
--- src/test/java/com/example/ContactsAPI/ContactsEndpointTests.java
+++ src/test/java/com/example/ContactsAPI/ContactsEndpointTests.java
@@ -3,11 +3,8 @@ package com.example.ContactsAPI;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -19,22 +16,13 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ContactsEndpointTests {
-    private static final String API_ROOT = "http://localhost/api/contacts";
-
-    @Autowired
-    ContactRepository contactRepo;
-
-    @LocalServerPort
-    private int port;
-
-    @BeforeEach
-    public void setPort() {
-        RestAssured.port = this.port;
-        contactRepo.deleteAll();
+class ContactsEndpointTests extends BaseEndpointTests<DBContact, ContactForCreation, ContactRepository> {
+    public ContactsEndpointTests() {
+        super("http://localhost/api/contacts");
     }
 
-    private ContactForCreation createRandomContact() {
+    @Override
+    protected ContactForCreation createRandomObject() {
         ContactForCreation contact = new ContactForCreation();
         contact.setFirstName(RandomStringUtils.randomAlphabetic(10));
         contact.setLastName(RandomStringUtils.randomAlphabetic(10));
@@ -44,34 +32,20 @@ class ContactsEndpointTests {
         return contact;
     }
 
-    private String getContactUrl(Response response) {
-        return response.getHeader("location");
-    }
+    @Override
+    protected ContactForCreation createInvalidObject() {
+        ContactForCreation contact = createRandomObject();
+        contact.setEmail(RandomStringUtils.randomAlphabetic(10));
 
-    private String getRandomContactUrl() {
-        return API_ROOT + "/" + RandomStringUtils.randomNumeric(10);
+        return contact;
     }
 
     // C
-    @Test
-    void createValid_contact_then201() {
-        // Given
-        ContactForCreation contact = createRandomContact();
-
-        // When
-        Response createResponse = RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact)
-                .post(API_ROOT);
-
-        // Then
-        assertEquals(HttpStatus.CREATED.value(), createResponse.getStatusCode());
-    }
 
     @Test
-    void createExistingEmail_contact_then409Conflict() {
+    void createExisting_email_then409Conflict() {
         // Given
-        ContactForCreation contact = createRandomContact();
+        ContactForCreation contact = createRandomObject();
 
         // When
         RestAssured.given()
@@ -85,134 +59,13 @@ class ContactsEndpointTests {
         assertEquals(HttpStatus.CONFLICT.value(), createResponse.getStatusCode());
     }
 
-    @Test
-    void createInvalid_contact_then400() {
-        // Given
-        ContactForCreation contact = createRandomContact();
-        contact.setEmail(RandomStringUtils.randomAlphabetic(10));
-
-        // When
-        Response createResponse = RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact).post(API_ROOT);
-
-        // Then
-        assertEquals(HttpStatus.BAD_REQUEST.value(), createResponse.getStatusCode());
-    }
-
-    // R
-    @Test
-    void readFound_singleContact_then200() {
-        // Given
-        ContactForCreation contact = createRandomContact();
-        Response createResponse = RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact).post(API_ROOT);
-
-        // When
-        Response getResponse = RestAssured.get(getContactUrl(createResponse));
-
-        // Then
-        assertEquals(HttpStatus.OK.value(), getResponse.getStatusCode());
-        assertEquals(contact.getEmail(), getResponse.body().as(DBContact.class).getEmail());
-    }
-
-    @Test
-    void readMissing_singleContact_then404() {
-        // Given
-
-        // When
-        Response getResponse = RestAssured.get(getRandomContactUrl());
-
-        // Then
-        assertEquals(HttpStatus.NOT_FOUND.value(), getResponse.getStatusCode());
-    }
-
-    @Test
-    void readFound_contactList_then200() {
-        // Given
-        ContactForCreation contact = createRandomContact();
-        ContactForCreation contact2 = createRandomContact();
-        RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact).post(API_ROOT);
-        RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact2).post(API_ROOT);
-
-        // When
-        Response getResponse = RestAssured.get(API_ROOT);
-
-        // Then
-        assertEquals(HttpStatus.OK.value(), getResponse.getStatusCode());
-        assertEquals(2, getResponse.body().as(DBContact[].class).length);
-    }
-
-    @Test
-    void readNone_contactList_then204() {
-        // Given
-
-        // When
-        Response getResponse = RestAssured.get(API_ROOT);
-
-        // Then
-        assertEquals(HttpStatus.NO_CONTENT.value(), getResponse.getStatusCode());
-    }
-
     // U
-    @Test
-    void updateWithValid_contact_then200() {
-        // Given
-        ContactForCreation contact = createRandomContact();
-        ContactForCreation contact2 = createRandomContact();
-
-        // When
-        Response createResponse = RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact).post(API_ROOT);
-        Response updateResponse = RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact2).put(getContactUrl(createResponse));
-
-        // Then
-        assertEquals(HttpStatus.OK.value(), updateResponse.getStatusCode());
-        assertEquals(contact2.getEmail(), updateResponse.body().as(DBContact.class).getEmail());
-    }
-
-    @Test
-    void updateMissing_contact_then201() {
-        // Given
-        ContactForCreation contact = createRandomContact();
-
-        // When
-        Response updateResponse = RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact).put(getRandomContactUrl());
-
-        // Then
-        assertEquals(HttpStatus.CREATED.value(), updateResponse.getStatusCode());
-    }
-
-    @Test
-    void UpdateWithInvalid_contact_then400() {
-        // Given
-        ContactForCreation contact = createRandomContact();
-        contact.setEmail(RandomStringUtils.randomAlphabetic(10));
-
-        // When
-        Response updateResponse = RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact).put(getRandomContactUrl());
-
-        // Then
-        assertEquals(HttpStatus.BAD_REQUEST.value(), updateResponse.getStatusCode());
-    }
 
     @Test
     void updateWithExisting_email_then409() {
         // Given
-        ContactForCreation contact = createRandomContact();
-        ContactForCreation otherContact = createRandomContact();
+        ContactForCreation contact = createRandomObject();
+        ContactForCreation otherContact = createRandomObject();
 
         Response createResponse = RestAssured.given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -227,40 +80,9 @@ class ContactsEndpointTests {
 
         Response updateResponse = RestAssured.given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact).put(getContactUrl(createResponse));
+                .body(contact).put(getEntryUrl(createResponse));
 
         // Then
         assertEquals(HttpStatus.CONFLICT.value(), updateResponse.getStatusCode());
-    }
-
-    // D
-    @Test
-    void deleteExisting_contact_then200() {
-        // Given
-        ContactForCreation contact = createRandomContact();
-        Response createResponse = RestAssured.given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(contact).post(API_ROOT);
-
-        String contactLoc = getContactUrl(createResponse);
-
-        // When
-        Response deleteResponse = RestAssured.delete(contactLoc);
-
-        // Then
-        assertEquals(HttpStatus.OK.value(), deleteResponse.getStatusCode());
-        Response getResponse = RestAssured.get(contactLoc);
-        assertEquals(HttpStatus.NOT_FOUND.value(), getResponse.getStatusCode());
-    }
-
-    @Test
-    void deleteMissing_contact_then404() {
-        // Given
-
-        // When
-        Response deleteResponse = RestAssured.delete(getRandomContactUrl());
-
-        // Then
-        assertEquals(HttpStatus.NOT_FOUND.value(), deleteResponse.getStatusCode());
     }
 }
